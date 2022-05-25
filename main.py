402945e7aa8daff361cfc64b9ad66950958d503b
Gideon <balogungideon12@gmail.com>
2022-05-25 17:05:39 +0100

    update

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-# Check if a word is an anagrams 
+# Check if two words are anagrams
 # Example:
-# find_anagrams("hello") --> False
-# find_anagrams("racecar") --> True
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
 
 
-def find_anagrams(word):
+def find_anagram(word, anagram):
     # [assignment] Add your code here
-    if word[::-1] == word:
-        return True
+    if len(word) == len(anagram):
+        count = 0
+        for i in anagram:
+            if i in word:
+                count +=1
+        if count == len(word):
+            return True
+
     return False
 
+print(find_anagram("kill", "lik"))
